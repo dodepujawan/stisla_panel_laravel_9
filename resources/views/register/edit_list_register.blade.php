5cd1e846d2271f3cee4be3632fde23a7b3d1c696
dodepujawan <122686500+dodepujawan@users.noreply.github.com>
2024-08-07 11:01:25 +0800

    ganti sweedtalert cdn

--- resources/views/register/edit_list_register.blade.php
+++ resources/views/register/edit_list_register.blade.php
@@ -34,7 +34,8 @@
     </div>
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+{{-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> --}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 <script>
 $(document).ready(function(){
 
